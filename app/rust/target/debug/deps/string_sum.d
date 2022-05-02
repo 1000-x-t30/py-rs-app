3afe173828d9644f7414999a32aab22bb6098433
1000-x-t30 <nagatomi.takayuki81@gmail.com>
2022-05-02 15:32:10 +0900

    rust環境修正

--- app/rust/target/debug/deps/string_sum.d
+++
@@ -1,5 +0,0 @@
-/usr/src/app/rust/target/debug/deps/libstring_sum.so: src/lib.rs
-
-/usr/src/app/rust/target/debug/deps/string_sum.d: src/lib.rs
-
-src/lib.rs:
--- app/rust/target/debug/libstring_sum.d
+++
@@ -1,1 +0,0 @@
-/usr/src/app/rust/target/debug/libstring_sum.so: /usr/src/app/rust/src/lib.rs
